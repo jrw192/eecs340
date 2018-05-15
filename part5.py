0e6a4b035ca5120e2bf09dff99cdea5192f14f26
jrw192 <jodiewei2019@u.northwestern.edu>
2018-05-14 22:03:14 -0500

    idk what's going on with part 5, got the "create own file" but that's it

--- part5.py
+++ part5.py
@@ -12,46 +12,59 @@ class HttpServer:
 		#bind the socket to the port and listen for connections 
 		self.s.bind(("", self.port))
 		self.s.listen(1)
+		
+		while True:
+			#part A: accept a new connection
+			connSocket, address = self.s.accept()
+			print "socket address: ", address
 
-		#part A: accept a new connection
-		connSocket, address = self.s.accept()
-		print "socket address: ", address
+			#part B: read HTTP request from connectionSocket and parse it
+			request = connSocket.recv(1024)
+			print "**request**"
+			print request
+			print "***********"
 
-		#part B: read HTTP request from connectionSocket and parse it
-		request = connSocket.recv(1024)
-		print "**request**"
-		print request
-		print "***********"
+			requestedFile = self.getReqFile(request)
+			print "**requested file: ", requestedFile
 
-		requestedFile = self.getReqFile(request)
-		print "**requested file: ", requestedFile
+			hostName = (self.getHost(request)).strip()
+			print "**hostname: ", hostName
+			absFile = str(os.path.abspath(requestedFile)).strip()
+			if self.isHostValid(hostName) and os.path.isfile(absFile):
+				#construct HTTP response if file exists
+				fileContent = ""
+				print "opening file...."
+				file = open(absFile, "rb")
+				fileContent = file.read()
+				file.close()
+				headerContent = self.makeHeader(requestedFile)
+				print "***header***"
+				print headerContent
+				print "************"
+				#print "***fileContent"
+				#print fileContent
+				#print "************"
+				connSocket.send((headerContent + fileContent))
+				#close when done
+				self.s.close()
 
-		#part C/D: construct HTTP response if file exists
-		fileContent = ""
-		absFile = str(os.path.abspath(requestedFile)).strip()
-		if os.path.isfile(absFile):
-			print "opening file...."
-			file = open(absFile, "rb")
-			fileContent = file.read()
-			file.close()
+			else:
+				print "we made our own page"
+				self.writeOurFile(hostName)
+				
+				date = datetime.datetime.now()
+				date = date.strftime("%d/%m/%Y %H:%M:%S")
+				dateResponse = "Date: " + date + "\r\n"
+				typeResponse = "Content-Type: text/html\r\n"
+				headerContent = "HTTP/1.1 200 OK\r\n" + dateResponse + typeResponse
 
-		#elif !os.path.isfile(absFile) and "htm" in absFile:
-		#	self.writeOurFile()
-			
-		headerContent = self.makeHeader(requestedFile)
-		print "***header***"
-		print headerContent
-		print "************"
+				file = open("ourFile", "rb")
+				fileContent = file.read()
+				file.close()
 
-		#idk
-		pageContent = self.writeOurFile(requestedFile) #?????
+				connSocket.send((headerContent + fileContent))
+				self.s.close()
 
-		#print "***fileContent"
-		#print fileContent
-		#print "************"
-		connSocket.send((headerContent + fileContent + pageContent))
-		#close when done
-		self.s.close()
 
 	def makeHeader(self, requestedFile):
 		date = datetime.datetime.now()
@@ -63,9 +76,9 @@ class HttpServer:
 		print os.path.abspath(requestedFile)
 		print len(str(os.path.abspath(requestedFile)).strip())
 		print len('/home/jrw192/project1/rfc2616.html')
-		if "htm" not in requestedFile:
-			headerContent = "HTTP/1.1 403 Forbidden\r\n" + headerContent
-		elif os.path.isfile(str(os.path.abspath(requestedFile)).strip()):	#this works on murphy
+		#if "htm" not in requestedFile:
+		#	headerContent = "HTTP/1.1 403 Forbidden\r\n" + headerContent
+		if os.path.isfile(str(os.path.abspath(requestedFile)).strip()):	#this works on murphy
 			#print "byteArray: ", list(bytearray((os.path.abspath(requestedFile).strip())))
 			headerContent = "HTTP/1.1 200 OK\r\n" + headerContent
 		else: 
@@ -78,18 +91,30 @@ class HttpServer:
 		requestedFile = request[request.find(' /')+len(' /'):request.find('HTTP')]
 		return requestedFile
 
-  # idk about this
-	def writeOurFile(self, request):
+	def getHost(self, request):
+		hostName = request[request.find('Host: ')+len('Host: '):request.find('Connection')]
+		return hostName
 
+	def isHostValid(self, hostname):
+		try:
+			socket.gethostbyname(hostname)
+			return True
+		except socket.error:
+			return False
 
+	
+	def writeOurFile(self, hostName):
+		file = open("ourFile", "w")
 		pageContent = """<!DOCTYPE html> 
 		<html> 
 		<body> 
-		<p> I see that you were looking for """ + whateverurlitis + """, but wouldn't you rather
+		<p> I see that you were looking for """ + hostName + """, but wouldn't you rather
 		buy that from <a href = "google.com"> google.com </a>? </p> 
 		</body> 
 		</html>"""
-		return pageContent
+		file= open("ourFile","w")
+		file.write(pageContent)
+		file.close()
 
 
 if  __name__ =='__main__':  
